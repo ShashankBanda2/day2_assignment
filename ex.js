bd0a4eedb45f8987d1852a8d0da05ad37030bdcb
ShashankBanda2 <shashankbanda2@gmail.com>
2021-09-28 17:13:01 +0530

    Initial commit

---
+++ ex.js
@@ -0,0 +1,24 @@
+function showtext()
+{
+    console.log('user clicked button1')
+}
+
+function mousedow()
+{
+    console.log('dont move down')
+}
+
+function mouseent()
+{
+    console.log('mouse entered')
+}
+
+function keydown()
+{
+    window.alert('dont press any key')
+}
+
+function drag()
+{
+    window.alert('dont drag')
+}
